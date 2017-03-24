ef80a34cd5d89ff2d794b268004a5c842e820a01
kumaroj <manishkr2207@gmail.com>
2017-03-24 15:39:55 +0530

    Add files via upload

---
+++ agename.java
@@ -0,0 +1,13 @@
+class agename
+{
+ public static void main(String []args)
+ {
+	display(24,"manish");
+	
+ }
+ static void display(int age,String name)
+{
+	System.out.println(age);
+	System.out.println(name);
+}
+}
